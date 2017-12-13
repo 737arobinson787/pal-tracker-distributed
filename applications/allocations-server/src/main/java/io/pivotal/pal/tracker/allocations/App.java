acace8dc7ed94de70bea643294e205f47f064648
Asim Robinson <asim.robinson@cognizant.com>
2017-12-12 22:18:14 -0800

    SSO

--- applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/App.java
+++ applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/App.java
@@ -3,17 +3,22 @@ package io.pivotal.pal.tracker.allocations;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.client.RestOperations;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+
 import java.util.TimeZone;
 
 @EnableCircuitBreaker
 @SpringBootApplication
 @ComponentScan({"io.pivotal.pal.tracker.allocations", "io.pivotal.pal.tracker.restsupport"})
 @EnableEurekaClient
+@EnableWebSecurity
+@EnableResourceServer
 public class App {
 
     public static void main(String[] args) {
@@ -29,3 +34,7 @@ public class App {
         return new ProjectClient(restOperations, registrationEndpoint);
     }
 }
+
+
+
+
